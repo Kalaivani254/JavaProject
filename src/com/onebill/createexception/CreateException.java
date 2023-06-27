ff9d71c192a0d124f6274a253ded5c2693b3fa92
Kannigakalai <kanniga.p15pg@gmail.com>
2023-06-28 01:13:23 +0530

    exception creation

---
+++ src/com/onebill/createexception/CreateException.java
@@ -0,0 +1,13 @@
+package com.onebill.createexception;
+
+public class CreateException {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=0;
+		int d=a/b;
+		System.out.println(d);
+
+	}
+
+}
